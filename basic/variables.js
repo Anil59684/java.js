f46a5513fc8d019fbcc696326ceb6209424477c5
Anil59684 <87606942+Anil59684@users.noreply.github.com>
2023-09-10 12:53:23 +0000

    learned about consst let and var

---
+++ basic/variables.js
@@ -0,0 +1,15 @@
+const accountId = 154846
+let accountEmail = "anil@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ap"
+
+// accountId = 2
+
+accountEmail = "sunil@gmail.com"
+accountPassword = "0000"
+accountCity = "sjh"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
