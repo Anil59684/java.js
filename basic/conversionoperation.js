85c782d1dc6e817af05872ed9a0ce98b635cd047
Anil59684 <87606942+Anil59684@users.noreply.github.com>
2023-09-11 06:37:57 +0000

    conversion of datatypes

---
+++ basic/conversionoperation.js
@@ -0,0 +1,21 @@
+"use strict";
+
+   // "33" => 33
+   // "33abcj => NaN"
+   // true => 1; false => 2;
+
+//    let isLoggedIn = 1
+
+//    let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//    console.log(booleanIsLoggedIn);
+
+//    console.log();
+
+
+   let someNumber = 33
+   
+   let stringNumber = String(someNumber);
+
+   console.log(stringNumber);
+   console.log(typeof stringNumber);
