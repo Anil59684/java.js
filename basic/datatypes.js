e1ede8fa468867370196b69dc1d92bb9e109826f
Anil59684 <87606942+Anil59684@users.noreply.github.com>
2023-09-11 06:19:46 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alter( 3 + 3 ); // we are using node.js, not browser
+
+console.log(3 
+    + 3); // code readability is important
+
+    // types of datatypes
+    // number => 2 to power
+    // bigint
+    //string => ""
+    // boolen => true/ false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+
+    // object
+
+    //null => object
