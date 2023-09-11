313b55e9d772c6ce3bfc2811132d4f4d23f953ea
Anil59684 <87606942+Anil59684@users.noreply.github.com>
2023-09-11 11:39:16 +0000

    learn about string

---
+++ basic/06_string.js
@@ -0,0 +1,36 @@
+const name = "Anil"
+const repoCount = 2
+
+//console.log(name + repoCount + " Value");
+
+//console.log(`Hello my name is ${name} `)
+
+const gameName = new String ('contra-pc-com')
+
+// console.log(gameName[2]);
+console.log(gameName.__prot__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+
+// console.log(newString);
+
+// const oppString = gameName.slice(-7, 4)
+// console.log(oppString);
+
+const newStringOne = "    Anil"
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+const url = "https://anil.com/anil=anime/onepiece/luffy"
+
+console.log(url.replace('=', '/'));
+console.log(url.includes('luffy'))
+
+console.log(gameName.split('-'))
