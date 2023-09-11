102225e49e61ba3f637321f322fc077371acfd53
Anil59684 <87606942+Anil59684@users.noreply.github.com>
2023-09-11 10:30:45 +0000

    datatypes summary

---
+++ basic/dtattype-summary.js
@@ -0,0 +1,19 @@
+// two types of data type
+
+// 1. primitive datatypes  
+// String, number, Boolean, null, undefined, symbol, bigint,
+
+// javascript is dynamic type languages
+
+//2. reference type or nonprimitivee
+
+// Array, object, function,
+
+
+// function datatype is objectfunction
+// Array typeof is function
+// object typeof is function
+
+
+
+
