63d5932c257ddbd89c8cf7bf34659cc454b81886
Anil59684 <87606942+Anil59684@users.noreply.github.com>
2023-09-11 07:03:03 +0000

    operation

--- basic/conversionoperation.js
+++ basic/conversionoperation.js
@@ -1,21 +1,42 @@
 "use strict";
 
-   // "33" => 33
-   // "33abcj => NaN"
-   // true => 1; false => 2;
+//  "33" => 33
+// "33abcj => NaN"
+// true => 1; false => 2;
 
-//    let isLoggedIn = 1
+   let isLoggedIn = 1
 
-//    let booleanIsLoggedIn = Boolean(isLoggedIn);
+   let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 //    console.log(booleanIsLoggedIn);
-
-//    console.log();
-
 
    let someNumber = 33
    
    let stringNumber = String(someNumber);
 
-   console.log(stringNumber);
-   console.log(typeof stringNumber);
+//    console.log(stringNumber);
+//    console.log(typeof stringNumber);
+
+
+// *********************Operation************************
+
+let value = 2
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2) plus
+// console.log(2-2) substraction
+// console.log(2*2) multiply
+// console.log(2**2) power
+// console.log(2/2) divide
+// console.log(2%2) remainder or modulus
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
